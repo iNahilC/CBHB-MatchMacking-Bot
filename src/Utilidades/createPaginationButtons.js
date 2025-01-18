8631d786d63d5fcabdd5fced002d325b16cee202
Nahil R.M. <43559837+iNahilC@users.noreply.github.com>
2025-01-17 23:44:54 -0400

    /tabla command & Pagination Buttons Function.
    Nuevo comando /tabla para visualizar lista de todos los jugadores que contengan ELO.
    
    Nueva función para crear botones de paginación para los embeds/mensajes.

---
+++ src/Utilidades/createPaginationButtons.js
@@ -0,0 +1,18 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+
+function createPaginationButtons(paginaActual, paginasTotal) {
+  return new ActionRowBuilder().addComponents(
+    new ButtonBuilder()
+      .setCustomId("prev_pagina")
+      .setLabel("←")
+      .setStyle(ButtonStyle.Primary)
+      .setDisabled(paginaActual === 0),
+    new ButtonBuilder()
+      .setCustomId("next_pagina")
+      .setLabel("→")
+      .setStyle(ButtonStyle.Primary)
+      .setDisabled(paginaActual === paginasTotal - 1)
+  );
+}
+
+module.exports = { createPaginationButtons };
